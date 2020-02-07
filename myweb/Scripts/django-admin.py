fea621faba2a64de2aa8699d2b3599abe2406b3b
manish <mkmaurya955@gmail.com>
2020-02-07 14:49:46 +0530

    atm

---
+++ myweb/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sai\desktop\manish\python\e-commerce\myweb\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
